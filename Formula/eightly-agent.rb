1fb65056ae65ab50d80c83df069730d2f4c73197
smashingtags <smashingtags@users.noreply.github.com>
2026-06-01 02:48:13 -0400

    INFRA-70: repoint ghcr.io + github.com refs to imogenlabs org

--- Formula/eightly-agent.rb
+++ Formula/eightly-agent.rb
@@ -6,22 +6,22 @@ class EightlyAgent < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/smashingtags/eightly-agent/releases/download/v1.0.0-beta1/eightly-agent-darwin-arm64"
+      url "https://github.com/imogenlabs/eightly-agent/releases/download/v1.0.0-beta1/eightly-agent-darwin-arm64"
       sha256 "b7f0fc04143e199694127bf8c16a0fd063f170953039e2008d22599f82abc936"
     end
     on_intel do
-      url "https://github.com/smashingtags/eightly-agent/releases/download/v1.0.0-beta1/eightly-agent-darwin-amd64"
+      url "https://github.com/imogenlabs/eightly-agent/releases/download/v1.0.0-beta1/eightly-agent-darwin-amd64"
       sha256 "0f4b4f0c84f88ecaee69b3f69b5b82a34d108db7d61b69b2524dd5299d2c2d4b"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/smashingtags/eightly-agent/releases/download/v1.0.0-beta1/eightly-agent-linux-arm64"
+      url "https://github.com/imogenlabs/eightly-agent/releases/download/v1.0.0-beta1/eightly-agent-linux-arm64"
       sha256 "075441de667d1c1026a59b0f91dca521b92f76b8de3a83d3feb718f692dba691"
     end
     on_intel do
-      url "https://github.com/smashingtags/eightly-agent/releases/download/v1.0.0-beta1/eightly-agent-linux-amd64"
+      url "https://github.com/imogenlabs/eightly-agent/releases/download/v1.0.0-beta1/eightly-agent-linux-amd64"
       sha256 "c4bcd7ee2b943f8060f87864ac145a55998f56274883030c8bd1b18677614bdf"
     end
   end
